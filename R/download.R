6adcb389b3303cfa32383859f898b10a473ba7a9
chainsawriot <chainsawtiney@gmail.com>
2021-12-15 10:21:02 +0100

    Make download.file mode explicit for Windows

--- R/download.R
+++ R/download.R
@@ -6,7 +6,7 @@
     message("DB doesn't exist. Attempting to download it from the Internet.")
     message("Make sure you agree with the WordNet License.")
     temploc <- tempfile("sqlite-31.db.zip")
-    utils::download.file("http://downloads.sourceforge.net/project/wnsql/wnsql3/sqlite/3.1/sqlite-31.db.zip?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fwnsql%2Ffiles%2Fwnsql3%2Fsqlite%2F3.1%2Fsqlite-31.db.zip%2Fdownload&ts=1409711250&use_mirror=iweb", temploc, quiet = FALSE)
+    utils::download.file("http://downloads.sourceforge.net/project/wnsql/wnsql3/sqlite/3.1/sqlite-31.db.zip?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fwnsql%2Ffiles%2Fwnsql3%2Fsqlite%2F3.1%2Fsqlite-31.db.zip%2Fdownload&ts=1409711250&use_mirror=iweb", temploc, quiet = FALSE, mode = "wb")
     utils::unzip(temploc, files = c("sqlite-31.db"), exdir = system.file(package = "sehrnett"))
     unlink(temploc)
     invisible(TRUE)
